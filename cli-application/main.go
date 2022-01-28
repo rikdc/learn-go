24761112c94b820e7c09d309019a63cdfe7ab342
Richard Claydon <rclaydon@wayfair.com>
2022-01-28 09:51:05 -0500

    Added log reading logic.

--- cli-application/main.go
+++ cli-application/main.go
@@ -1,5 +1,32 @@
 package main // All go applications start with a package.
 
-func main() { // keyboard shortcut = fm<tab>
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
+func main() { // keyboard shortcut = fm<tab>
+	f, err := os.Open("myapp.log") // Saving added import automatically. Huzzah!
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// defer, after the main function exists will close the file automatically
+	defer f.Close()
+
+	// reading contents of the file.
+	r := bufio.NewReader(f)
+
+	for { // infinite for loop
+		s, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+		if strings.Contains(s, "ERROR") {
+			fmt.Println(s)
+		}
+	}
 }
