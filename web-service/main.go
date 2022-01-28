878e2a444edc1b2e60298a53ac0b012fb2d3980b
Richard Claydon <rclaydon@wayfair.com>
2022-01-28 10:06:43 -0500

    Insecure param handling.

--- web-service/main.go
+++ web-service/main.go
@@ -7,7 +7,13 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("Hello World"))
+		names := r.URL.Query()["name"]
+		var name string
+		if len(names) == 1 {
+			name = names[0]
+		}
+
+		rw.Write([]byte("Hello " + name)) // XSS!!
 	})
 
 	err := http.ListenAndServe(":3000", nil)
