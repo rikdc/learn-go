b5bb00e110b96c5cce35a4f227d6ba143ff5fc56
Richard Claydon <rclaydon@wayfair.com>
2022-02-07 09:50:21 -0500

    Array fun.

---
+++ arrays/go.mod
@@ -0,0 +1,3 @@
+module github.com/pluralsight/arrays
+
+go 1.17
---
+++ arrays/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Basic array usage
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	fmt.Println(arr)
+
+}
