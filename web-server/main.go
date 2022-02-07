4296fe71df998809247344a96ab53eddcadbecd1
Richard Claydon <rclaydon@wayfair.com>
2022-02-07 14:48:12 -0500

    Experimenting with return values.

--- web-server/main.go
+++ web-server/main.go
@@ -13,4 +13,15 @@ func main() {
 		LastName:  "Pan",
 	}
 	fmt.Println(u)
+
+	port := 3000
+	_, err := startWebServer(port)
+	fmt.Println(err)
+}
+
+func startWebServer(port int) (int, error) {
+	fmt.Println("Starting webserver...")
+	fmt.Println("Server started on port", port)
+
+	return port, nil
 }
