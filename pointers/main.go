e33d0694f2b6afe7dd1d25939a5775ce001119d1
Richard Claydon <rclaydon@wayfair.com>
2022-02-07 09:24:08 -0500

    fun with pointers

--- pointers/main.go
+++ pointers/main.go
@@ -2,9 +2,24 @@
 // Click here and start typing.
 package main
 
+import "fmt"
+
 func main() {
 
 	// Declare the pointer. This will be unintialized.
-	var firstName *string
+	//var firstName *string
+
+	// This will error out.
+	// firstName = "Arthur"
+
+	// Dereferencing the pointer will also generate an error.
+	// *firstName = "Arthur"
+
+	// Correct: Initialize at create
+	// Declare the pointer. This will be initialized.
+	var firstName *string = new(string)
+	*firstName = "Arthur"
+
+	fmt.Println(*firstName) // Remember to dereference!
 
 }
