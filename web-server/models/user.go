a83eb4790205c01eb8fbf9b9e0af2236df78a776
Richard Claydon <rclaydon@wayfair.com>
2022-02-08 08:14:47 -0500

    Implementing GetUser and AddUser functions.

--- web-server/models/user.go
+++ web-server/models/user.go
@@ -10,3 +10,15 @@ var (
 	users  []*User // A slice of pointers to user objects.
 	nextId = 1
 )
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(u User) (User, error) {
+	u.ID = nextId
+	nextId++
+	users = append(users, &u)
+
+	return u, nil
+}
