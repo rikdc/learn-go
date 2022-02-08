746c753942d36b4d38816066a3f18355fe36b3f9
Richard Claydon <rclaydon@wayfair.com>
2022-02-08 08:35:15 -0500

    Adding a constructor to the user controller.

--- web-server/controllers/user.go
+++ web-server/controllers/user.go
@@ -1,9 +1,26 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type userController struct{}
+type userController struct {
+	userIdPattern *regexp.Regexp
+}
 
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World from the User Controller!"))
+}
+
+// Constructor function.
+// This uses a convention, not a rule.
+func newUserController() *userController {
+	// Returning the addressOf operator here is only applicable to structs.
+	// This is a local variable, and returning the address.
+	// C++ engineers may find this weird, but Go adds protections around this
+	// by promoting the local variables scope.
+	return &userController{
+		userIdPattern: regexp.MustCompile(`^/users/(\d+)/?`),
+	}
 }
